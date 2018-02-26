22ef3f5ca0da4b0993c099f666136d246b3e538c
z <z@Macintosh.local>
2018-02-26 01:28:46 -0800

    added the server file and removed css default styles

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 /**
  * Created by z on 2/26/18.
  */
+const express = require('express');
+const app = express();
+
+app.use(express.static('build'));
+app.set('views', './build');
+
+app.get('/', function(req, res){
+    res.sendFile(`${process.cwd()}/build/index.html`)
+});
+
+app.listen(2400, function(){
+    console.log('Example app listening on port 3000!')
+});
